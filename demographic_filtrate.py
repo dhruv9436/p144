d8a13f5c5ff5cac0e6aa3ea751b7e8d6eed265b1
dhruv9436 <78801354+dhruv9436@users.noreply.github.com>
2023-11-17 09:26:34 +0530

    Add files via upload

---
+++ demographic_filtrate.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+df = pd.read_csv("articles.csv")
+df_sorted = df.sort_values(by="total_events", ascending=False)
+
+top_articles = df_sorted.head(20).to_dict(orient="records")
